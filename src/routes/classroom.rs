4ba9bc2e5a8b83f8a8edc790780dd33081220cb9
Kayxue <s0990006@gmail.com>
2025-10-14 19:40:24 +0800

    Finished register user, list classrooms, create classroom route (Authorization not finished)

--- src/routes/classroom.rs
+++ src/routes/classroom.rs
@@ -1,27 +1,38 @@
 use axum::{Json, Router, extract::State, http::StatusCode, response::IntoResponse, routing::get};
 use entities::classroom;
 use nanoid::nanoid;
-use sea_orm::{ActiveModelTrait, ActiveValue::{NotSet, Set}, EntityTrait};
+use sea_orm::{
+    ActiveModelTrait,
+    ActiveValue::{NotSet, Set},
+    EntityTrait,
+};
+use serde::{Deserialize, Serialize};
 
-use crate::{entities::{self, sea_orm_active_enums::Status}, AppState};
+use crate::{
+    AppState,
+    entities::{self, sea_orm_active_enums::Status},
+};
 
-async fn list_classrooms(State(state): State<AppState>) -> impl IntoResponse {
-    match classroom::Entity::find().all(&state.db).await {
-        Ok(classrooms) => (StatusCode::OK, Json(classrooms)).into_response(),
-        Err(_) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Failed to fetch classrooms",
-        )
-            .into_response(),
-    }
+#[derive(Deserialize, Serialize)]
+struct CreateClassroomBody {
+    name: String,
+    capacity: i32,
+    location: String,
 }
 
-async fn create_classroom(State(state): State<AppState>) -> impl IntoResponse {
+async fn create_classroom(
+    State(state): State<AppState>,
+    Json(CreateClassroomBody {
+        name,
+        capacity,
+        location,
+    }): Json<CreateClassroomBody>,
+) -> impl IntoResponse {
     let new_classroom = classroom::ActiveModel {
         id: Set(nanoid!()),
-        name: Set("New Classroom".to_string()),
-        capacity: Set(30),
-        location: Set("電資大樓二樓".into()),
+        name: Set(name),
+        capacity: Set(capacity),
+        location: Set(location),
         status: Set(Status::Available),
         created_at: NotSet,
         updated_at: NotSet,
@@ -37,13 +48,23 @@ async fn create_classroom(State(state): State<AppState>) -> impl IntoResponse {
     }
 }
 
+async fn list_classrooms(State(state): State<AppState>) -> impl IntoResponse {
+    match classroom::Entity::find().all(&state.db).await {
+        Ok(classrooms) => (StatusCode::OK, Json(classrooms)).into_response(),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Failed to fetch classrooms",
+        )
+            .into_response(),
+    }
+}
+
 pub fn classroom_router() -> Router<AppState> {
-    Router::new()
-        .route("/classrooms", get(list_classrooms).post(create_classroom))
-        // .route(
-        //     "/classrooms/:id",
-        //     get(get_classroom)
-        //         .put(update_classroom)
-        //         .delete(delete_classroom),
-        // )
+    Router::new().route("/", get(list_classrooms).post(create_classroom))
+    // .route(
+    //     "/classrooms/:id",
+    //     get(get_classroom)
+    //         .put(update_classroom)
+    //         .delete(delete_classroom),
+    // )
 }
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,14 +1,61 @@
 use axum::{
     Json, Router,
+    extract::State,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
 };
+use axum_login::login_required;
+use sea_orm::{
+    ActiveModelTrait,
+    ActiveValue::{NotSet, Set},
+};
+use serde::{Deserialize, Serialize};
 
 use crate::{
     AppState,
-    loginsystem::{AuthSession, Credentials},
+    argonhasher::hash,
+    entities::{sea_orm_active_enums::Role, user},
+    loginsystem::{AuthBackend, AuthSession, Credentials},
 };
+
+use nanoid::nanoid;
+
+#[derive(Serialize, Deserialize)]
+struct RegisterBody {
+    username: String,
+    email: String,
+    password: String,
+    phone_number: String,
+}
+
+async fn register(
+    State(state): State<AppState>,
+    Json(RegisterBody {
+        username,
+        email,
+        password,
+        phone_number,
+    }): Json<RegisterBody>,
+) -> impl IntoResponse {
+    let hashed_password = hash(password.as_bytes()).await.unwrap();
+
+    let new_user = user::ActiveModel {
+        id: Set(nanoid!()),
+        username: Set(username),
+        email: Set(email),
+        password: Set(hashed_password),
+        phone_number: Set(phone_number),
+        role: Set(Role::User),
+        created_at: NotSet,
+        updated_at: NotSet,
+    };
+
+    match new_user.insert(&state.db).await {
+        Ok(user) => (StatusCode::CREATED, Json(user)).into_response(),
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Failed to create user").into_response(),
+    }
+}
 
 async fn login(mut auth_session: AuthSession, Json(body): Json<Credentials>) -> impl IntoResponse {
     let user = match auth_session.authenticate(body).await {
@@ -33,9 +80,8 @@ async fn logout(mut auth_session: AuthSession) -> impl IntoResponse {
     }
 }
 
-async fn profile() -> impl IntoResponse {
-    //TODO: Return user profile information
-    "User profile"
+async fn profile(session: AuthSession) -> impl IntoResponse {
+    (StatusCode::OK, Json(session.user.unwrap())).into_response()
 }
 
 pub fn user_router() -> Router<AppState> {
@@ -43,5 +89,9 @@ pub fn user_router() -> Router<AppState> {
     Router::new()
         .route("/login", post(login))
         .route("/logout", get(logout))
-        .route("/profile", get(profile))
+        .route(
+            "/profile",
+            get(profile).route_layer(login_required!(AuthBackend)),
+        )
+        .route("/register", post(register))
 }
